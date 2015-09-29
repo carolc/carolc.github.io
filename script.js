6deaafcb80c8238e93ebd55ddbe48672c5846536
carolc <carollyc@gmail.com>
2015-09-28 19:59:28 -0700

    buggy

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ $(document).ready(function(){
 			data: $(this).serialize(),
 			dataType: 'json',
 			beforeSend: function() {
-				$contactForm.append('<div class="alert alert-loading">Sending…</div>');
+				$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
 			},
 			success: function(data) {
 				$contactForm.find('.alert--loading').hide();
-				$contactForm.append('<div class="alert alert-success">Message sent! I will get back to you soon!</div>');
+				$contactForm.append('<div class="alert alert--success">Message sent!</div>');
 			},
 			error: function(err) {
 				$contactForm.find('.alert--loading').hide();
-				$contactForm.append('<div class="alert alert-error">Oops, there was an error.</div>');
+				$contactForm.append('<div class="alert alert--error">Oops, there was an error.</div>');
 			}
 		});
 	});
